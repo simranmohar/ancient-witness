860c6b61e9d9cbb38542b5a0f97d87bea582ee19
Simran Mohar <77524871+simranmohar@users.noreply.github.com>
2026-02-16 16:06:11 -0800

    fix projectId and create .env.local

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "8Ss5sZK8Doo", // This is your ID from earlier
+  projectId: "iiytr5i4", // This is your ID from earlier
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false, // False means we get fresh data instantly (good for dev)
