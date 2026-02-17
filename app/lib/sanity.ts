deaee935a9e40343b5c98b0467fcbd1b4f2c2d9f
Simran Mohar <77524871+simranmohar@users.noreply.github.com>
2026-02-16 16:13:29 -0800

    update sanity connection to properly use env variables

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,18 @@
 import { createClient } from "next-sanity";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
+
+if (!projectId || !dataset || !apiVersion) {
+  throw new Error(
+    "Missing Sanity configuration. Please check your environment variables."
+  );
+}
+
 export const client = createClient({
-  projectId: "iiytr5i4", // This is your ID from earlier
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false, // False means we get fresh data instantly (good for dev)
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false, // False means we get fresh data instantly but slower, true means we get catched data but might be 60 seconds old.
 });
